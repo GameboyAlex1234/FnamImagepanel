fe209572cc06e5185104ad878a54a7bd983c4572
GameboyAlex1234 <gameboyalex1234@gmail.com>
2025-10-08 12:28:33 +0200

    Delete netlify/functions/checkCode.js

--- netlify/functions/checkCode.js
+++
@@ -1,9 +0,0 @@
-export async function handler(event) {
-  const { code } = JSON.parse(event.body || '{}');
-
-  if (code === process.env.REQUIRED_CODE) {
-    return { statusCode: 200, body: JSON.stringify({ success: true }) };
-  } else {
-    return { statusCode: 401, body: JSON.stringify({ success: false }) };
-  }
-}
